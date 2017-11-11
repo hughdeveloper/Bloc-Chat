5fbef64db42ee4335f77704270b86ea6add2367b
hughdeveloper <hbethell@outlook.com>
2017-11-10 22:47:17 -0800

    checkpoint 5

--- app/scripts/controllers/BlocChatCookiesCtrl.js
+++ app/scripts/controllers/BlocChatCookiesCtrl.js
@@ -2,13 +2,16 @@
   function BlocChatCookiesCtrl($uibModalInstance, $cookies, $uibModal) {
     var currentUser = $cookies.get('blocChatCurrentUser');
 
+
+
     this.close = function() {
       var username = this.username;
       console.log(username);
-        // if the currentUser exsists it will close the username form
-        if (username){
-          $uibModalInstance.close();
-        }
+
+      // if the currentUser exsists it will close the username form
+      if (username) {
+        $uibModalInstance.close();
+      }
 
       //updates the browser cookies to set blocChatCurrentUser to the username
       $cookies.put('blocChatCurrentUser', username);
@@ -17,5 +20,5 @@
 
   angular
     .module('blocChat')
-    .controller('BlocChatCookiesCtrl', ['$uibModalInstance','$cookies', '$uibModal', BlocChatCookiesCtrl]);
+    .controller('BlocChatCookiesCtrl', ['$uibModalInstance', '$cookies', '$uibModal', BlocChatCookiesCtrl]);
 })();
